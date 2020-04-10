23ba768690fd9768d588e32042c9dead7bb013ca
Oz Michaeli <oz@appfigures.com>
2020-04-10 10:34:39 -0400

    If the baseUrl contains a protocol ignore the protocol option.

--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
 require('chai').should()
 var emoji = require('../')
 
-describe('react-easy-emoji', function () {
-	it ('should work', function () {
-
+describe('react-easy-emoji', () => {
+	it ('should work', () => {
 		var elements = emoji('This is really 😄')
 
 		elements.should.have.lengthOf(2)
@@ -15,5 +14,9 @@ describe('react-easy-emoji', function () {
 		element.props.alt.should.equal('😄')
 		element.key.should.equal('15')
 		element.props.src.should.equal('//twemoji.maxcdn.com/2/72x72/1f604.png')
+	})
+	it ('should ignore the protocol option if one is provided in the baseUrl', () => {
+		var element = emoji('😄', { baseUrl: 'https://someurl.com/'})[0]
+		element.props.src.should.equal('https://someurl.com/72x72/1f604.png')
 	})
 })
