0d469f07dfb349ab53433d657397c5e7e0808eaf
daniel <34781332+vogeldaniel@users.noreply.github.com>
2023-01-31 14:03:18 -0800

    add failing test

--- test/test.js
+++ test/test.js
@@ -24,4 +24,17 @@ describe('react-easy-emoji', () => {
 		var element = emoji(['😄', '😄'])
 		element[0].key.should.not.equal(element[1].key)
 	})
+
+	it ('should support emoji 14', () => {
+		var element = emoji('🫠')
+		element.should.have.lengthOf(1)
+		element[0].type.should.equal('img')
+		element[0].props.alt.should.equal('🫠')
+		element[0].props.src.should.equal('//twemoji.maxcdn.com/2/72x72/1fae0.png')
+	})
+
+	it ('handles empty size params to produce svgs', () => {
+		var element = emoji('😄', { size: '' })[0]
+		element.props.src.should.equal('//twemoji.maxcdn.com/2/svg/1f604.svg')
+	})
 })
